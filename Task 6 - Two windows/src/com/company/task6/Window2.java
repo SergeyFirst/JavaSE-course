926fb38b9b9dfc45f4d42659385d55af8be856b4
SergeyFirst <soochkov.sergey@gmail.com>
2016-06-19 15:24:10 +0300

    Add Task 6 - Two windows

--- Task 6 - Two windows/src/com/company/task6/Window2.java
+++ Task 6 - Two windows/src/com/company/task6/Window2.java
@@ -29,31 +29,30 @@ class myComponentListener implements ComponentListener {
 
     @Override
     public void componentMoved(ComponentEvent e) {
-
         try {
             checkBoundary((JFrame) ((ComponentEvent) e).getSource());
         } catch (Exception e1) {
             e1.printStackTrace();
         }
-
     }
 
     @Override
     public void componentShown(ComponentEvent e) {
-
     }
 
     @Override
     public void componentHidden(ComponentEvent e) {
-
     }
 
     public void checkBoundary(JFrame win) throws Exception {
+        //Определим разрешение экрана
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        //Проверим выход за границы экрана
         if ((win.getX() < 0) || (win.getY() < 0) || (win.getX() + win.getWidth() > screenSize.width)
                 || (win.getY() + win.getHeight() > screenSize.height) ) {
-            JOptionPane.showMessageDialog(null, "Достигнута максималная граница снизу");
-            throw new Exception("Достигнута максималная граница снизу");
+            JOptionPane.showMessageDialog(null, "Достигнута максималная граница экрана");
+            throw new Exception("Достигнута максималная граница экрана");
         }
     }
 }
